4d15cc0ec1d683a41272faca3f5abc851e2458d7
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-06-27 22:33:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,11 +5,13 @@ class Application
     resp = Rack::Response.new
     resp.write "#{Time.now.hour}\n"
 
-    if (Time.now.hour.to_i >= 12)
-      resp.write "Good Afternoon!"
-    elsif (Time.now.hour.to_i < 12)
-      resp.write "Good Morning!"
-    end
+    #if (Time.now.hour.to_i >= 12)
+    #  resp.write "Good Afternoon!"
+    #elsif (Time.now.hour.to_i < 12)
+    #  resp.write "Good Morning!"
+    #end
+
+    Time.now.hour.to_i >=12 ? resp.write "Good Afternoon!" : resp.write "Good Morning!"
 
     resp.finish
   end
