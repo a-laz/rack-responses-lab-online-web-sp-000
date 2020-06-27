92104d559f274ef7a5bd00c5bab765a42030f128
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-06-27 22:30:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
 
     resp = Rack::Response.new
-    resp.write Time.now.hour
+    resp.write "#{Time.now.hour}\n"
 
     if (Time.now.to_i >= 12)
       resp.write "Good Afternoon!"
